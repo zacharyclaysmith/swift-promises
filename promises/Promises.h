b57fce4feeeb1349422939bd9b4cff27177de239
Zach Smith <Zachary.Clay.Smith@gmail.com>
2015-02-02 15:36:28 -0800

    dded a missing file and a shared scheme

---
+++ promises/Promises.h
@@ -0,0 +1,19 @@
+//
+//  Promises.h
+//  Promises
+//
+//  Created by Zachary Smith on 2/2/15.
+//  Copyright (c) 2015 Zachary Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Promises.
+FOUNDATION_EXPORT double PromisesVersionNumber;
+
+//! Project version string for Promises.
+FOUNDATION_EXPORT const unsigned char PromisesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Promises/PublicHeader.h>
+
+
